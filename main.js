daf85329a4c42e0e8d3381ec1fb021de36f9310b
Natalia <nata_san@bk.ru>
2018-11-19 14:18:41 +0500

    First

---
+++ main.js
@@ -0,0 +1,27 @@
+//function random(number) {
+//    return Math.ceil(Math.random() * number);
+//}
+let name = prompt('Привет! Как тебя зовут?');
+
+while (name == '' || name == null) {
+    name = prompt('Привет! Как тебя зовут?');
+}
+
+alert(name + ', я загадал число от 1 до 100. Поробуй его отгадать за наименьшее количество попыток. После каждой попытки я скажу "мало" или "много" или "угадал".');
+
+let number = random(100);
+let guess = prompt('Какое число я загадал?');
+let numberOfGuesses = 0;
+
+while (guess != number) {
+    if (guess > number) {
+        guess = prompt('Много. Попробуй еще раз');
+        numberOfGuesses = numberOfGuesses + 1;
+    }
+    if (guess < number) {
+        guess = prompt('Мало. Попробуй еще раз');
+        numberOfGuesses = numberOfGuesses + 1;
+    }
+}
+
+alert('Ты угадал! Это число ' + number + '. Тебе понадобилось ' + numberOfGuesses + ' попыток.');
